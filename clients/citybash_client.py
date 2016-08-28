bad8fcdd0e0e10eb457d7f6f6e55d98b70c3d037
Leo Mehr <leo@hudson-trading.com>
2016-08-28 18:32:12 -0400

    citybash_client is always main

--- clients/citybash_client.py
+++ clients/citybash_client.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python
 import argparse
-import sys
 import utils
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser("Start a citybash client program")
+parser = argparse.ArgumentParser("Start a citybash client program")
 
-    parser.add_argument('host')
-    parser.add_argument('port', type=int)
-    parser.add_argument('player_key')
-    parser.add_argument('city_name_no_spaces')
-    parser.add_argument('-r', '--rejoin', action='store_true')
+parser.add_argument('host')
+parser.add_argument('port', type=int)
+parser.add_argument('player_key')
+parser.add_argument('city_name_no_spaces')
+parser.add_argument('-r', '--rejoin', action='store_true')
 
-    args = parser.parse_args()
-    key = args.player_key
-    name = args.city_name_no_spaces
+args = parser.parse_args()
+key = args.player_key
+name = args.city_name_no_spaces
 
-    do_setup = True
-    if args.rejoin:
-        do_setup = False
+do_setup = True
+if args.rejoin:
+    do_setup = False
 
-    if do_setup:
-        print 'Starting CityBash Game Client...'
-        utils.send_hello(args.host, args.port)
-        utils.send_join(args.host, args.port, key, name)
-    else:
-        print 'Resuming CityBash Game Client for key ' + key + '...'
+if do_setup:
+    print 'Starting CityBash Game Client...'
+    utils.send_hello(args.host, args.port)
+    utils.send_join(args.host, args.port, key, name)
+else:
+    print 'Resuming CityBash Game Client for key ' + key + '...'
 
-    print '*NOTE: your player key is automatically prepended to all messages*'
-    utils.run(args.host, args.port, key)
+print '*NOTE: your player key is automatically prepended to all messages*'
+utils.run(args.host, args.port, key)
