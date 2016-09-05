24bbfc544fa76f4293cb13154f280ae352004e0a
Leo Mehr <leo.mehr@gmail.com>
2016-09-05 19:49:49 -0400

    DOS client

---
+++ clients/dos_client.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+
+from utils import send_command
+import argparse
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('host')
+parser.add_argument('port', type=int)
+parser.add_argument('-d' '--duration', type=int)
+
+args = parser.parse_args()
+
+duration = args.d__duration or 3
+start = time.time()
+count = 0
+
+while True:
+    try:
+        msg = 'asdfngnqwrgwrkljadsnfiwngiwqeu023nksndjjjjjjjjjsdlfnsdjfnasdkjfnaskdjnfakjsdnfakjsdnfakjsdnfkajsdnfkasjdnfkjasndfkjansdkfjansdkjfnaskdjnfakjsdnfaskjdnf'
+        resp = send_command(args.host, args.port, msg)
+        count += 1
+
+        if time.time() - start > duration:
+            break
+    except:
+        break
+
+avg = float(count) / duration
+print 'Sent {} messages, with {} m/s'.format(count, avg)
